b6e190eae71375aca2bd84152515e649a14ac67c
GermanRodrickson <germanrodrickson@gmail.com>
2018-11-29 07:38:19 +0100

    remote api

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const request = require("request");
 
 app.get("/", (request, response, next) => {
   response.send("<h1>Welcome Ironhacker. :)</h1>");
+});
+
+request.get("http://www.mocky.io/v2/5808862710000087232b75ac", (error, response, body) => {
+  if (error) {
+    return console.dir(error);
+  }
+  console.dir(JSON.parse(body));
+});
+
+request.get("http://www.mocky.io/v2/580891a4100000e8242b75c5", (error, response, body) => {
+  if (error) {
+    return console.dir(error);
+  }
+  console.dir(JSON.parse(body));
 });
 
 app.listen(3000, () => {
