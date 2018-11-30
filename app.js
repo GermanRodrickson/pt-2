eb42a8ad43ccf5040100049e699865b425224436
GermanRodrickson <germanrodrickson@gmail.com>
2018-11-30 06:29:32 +0100

    request library

--- app.js
+++ app.js
@@ -2,24 +2,13 @@ const express = require("express");
 const app = express();
 const request = require("request");
 
-app.get("/", (request, response, next) => {
-  response.send("<h1>Welcome Ironhacker. :)</h1>");
-});
+request(
+  "http://www.mocky.io/v2/580891a4100000e8242b75c5",
+  (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    const data = JSON.parse(body)
 
-request.get("http://www.mocky.io/v2/5808862710000087232b75ac", (error, response, body) => {
-  if (error) {
-    return console.dir(error);
   }
-  console.dir(JSON.parse(body));
-});
-
-request.get("http://www.mocky.io/v2/580891a4100000e8242b75c5", (error, response, body) => {
-  if (error) {
-    return console.dir(error);
-  }
-  console.dir(JSON.parse(body));
-});
-
-app.listen(3000, () => {
-  console.log("My first app listening on port 3000!");
-});
+)
